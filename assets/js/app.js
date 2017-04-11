af54da8b56effa91527b240642821fbe1a5b5063
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-11 15:03:33 -0500

    Modal 90%

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,15 @@ var body = document.getElementById("body");
 var botonClose = document.getElementsByClassName("close");
 
 for(var i = 0; i < imagen.length; i++){
-  imagen[i].addEventListener("click", function (e) {
-    e.target.parentNode.parentNode.parentNode.classList.add("modal");
+  imagen[i].addEventListener("click", function(e) {
+    e.target.parentNode.parentNode.classList.add("modal");
     body.classList.add("overflow-hidden");
   })
 }
 
 for (var i = 0; i < botonClose.length; i++) {
   botonClose[i].addEventListener("click", function(e){
+    e.preventDefault();
     e.target.parentNode.parentNode.classList.remove("modal");
     body.classList.remove("overflow-hidden");
   })
